286cbd010c8f654b4542e650898b211aa56948b6
AP <112593668+codedAP@users.noreply.github.com>
2022-09-01 02:04:17 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_script 'c.lua'
+server_script 's.lua'
+
+files {
+	"index.html",
+	"js.js",
+	"option_1.png",
+	"option_2.png",
+	"option_3.png",
+	"dice_1.png",
+	"dice_2.png",
+	"dice_3.png",
+	"dice_4.png",
+	"dice_5.png",
+	"dice_6.png"
+}
+
+ui_page {
+	'index.html',
+}
